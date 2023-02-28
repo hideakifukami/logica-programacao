170680d7ec0e5c85029d777d0c39b938097bc50e
hideakifukami <denys.fukami@gmail.com>
2023-02-28 09:32:46 -0300

    create circle

--- first-project/src/Circle.java
+++ first-project/src/Circle.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+public class Circle {
     
     public static void main(String[] args) {
         double raio, area;
         final double PI = 3.1416;
 
         System.out.println("Escreva o valor do raio:");
-        Scanner scan = new Scanner(System.in);
-        raio = scan.nextDouble();
+        Scanner myObj = new Scanner(System.in);
+        raio = myObj.nextDouble();
         area = PI * Math.pow(raio, 2);
 
         System.out.print("O valor da area é " + area);
